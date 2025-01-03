d7e7d3cace2d62265d361abeafaa675f8ce80f19
Verena Schelling <vereschelling@googlemail.com>
2025-01-03 14:32:08 +0100

    page.js in comments auskommentiert

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+        <div className="body">
+            <div className="hero">
+              <img src="/hero-logo.png" alt="hero" className="herobild" />
+              <img src="/einkaufswagen.png" alt="wagen" className="wagen" />
+              <p className="hero-text">Werde Teil der Community!</p>
+              <button className="hero-button">Jetzt anmelden</button>
+            </div>
+        </div>
+    );
+}
