0c14178b22a74a43dd623fee572601f0686f0e74
annibabur <berivan.babur@student.reutlingen-university.de>
2025-01-05 17:09:19 +0100

    neue Komponente Community hinzugefügt

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Community from './components/Community';
 
 export default function Home() {
   return (
     <div className="body">
         <div className="Header"> <Header/> </div>
         <Hero/>
+        <Community/>
     </div>
   )
 }
