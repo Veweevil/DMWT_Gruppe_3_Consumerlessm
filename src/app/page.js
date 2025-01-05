e05fa3258eecb98004bd22279294ce2ebcf037a9
annibabur <berivan.babur@student.reutlingen-university.de>
2025-01-05 13:38:33 +0100

    Datenbank erfolgreich hinzugefügt und exzellente stysles verwendet

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 export default function Home() {
   return (
     <div className="body">
         <div className="Header"> <Header/> </div>
-        <div className="hero">
-          <img src="/hero-logo.png" alt="hero" className="herobild" />
-          <img src="/einkaufswagen.png" alt="wagen" className="wagen" />
-          <p className="hero-text">Werde Teil der Community!</p>
-          <button className="hero-button">Jetzt anmelden</button>
-        </div>
+        <Hero/>
     </div>
   )
 }
