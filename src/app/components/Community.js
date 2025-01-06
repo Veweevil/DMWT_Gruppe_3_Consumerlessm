3b6f9992179c6cef87a8cfdeb0589c6f80425dac
annibabur <berivan.babur@student.reutlingen-university.de>
2025-01-06 21:01:24 +0100

    Community

--- src/app/components/Community.js
+++ src/app/components/Community.js
@@ -5,7 +5,7 @@ export default function Community() {
     <h1 className="mb-10 font-trash-hand text-center text-7xl">Community</h1>
     <hr className="hr-mitte"/>
     <p className="mb-10 font-anonymous-pro text-center text-3xl italic">&bdquo;Entdecke die Community, die dich inspiriert&ldquo;</p>
-    <img className="mx-auto mb-10 rounded" src="community.jpg" alt="Peoples holding each others" width="800" height="300"></img>
+    <img className="mx-auto mb-10 rounded" src="community.jpg" alt="Peoples holding each others" width="600" height="250"></img>
     <div className="container">
         {/*Feld 1*/}
         <div className="item">
