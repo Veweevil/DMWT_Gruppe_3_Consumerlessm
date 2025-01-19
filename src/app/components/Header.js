02835c61c9512a932501e988171b9143ac1bbfeb
Efe Özkan <efe.oezkan@student.reutlingen-university.de>
2025-01-19 22:53:32 +0100

    logout in abmelden ändern

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -57,7 +57,7 @@ export default function Header() {
                             onClick={logout}
                             className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
                         >
-                            Logout
+                            Abmelden
                         </button>
                         
                     </>
